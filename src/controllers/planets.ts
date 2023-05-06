9b5598655283ffe676d757ff2a683f6d46025640
Krzysztof Pogorzelski <k.pogorzelski.dev@gmail.com>
2023-05-07 00:31:11 +0100

    simple add planets endpoint

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -3,11 +3,33 @@ import asyncHandler from "express-async-handler"
 import ErrorResponse from "../utils/ErrorResponse"
 import type { RequestWithCustomQuery } from "../middlewares/requestQueryBuilder"
 import { populate } from "../utils/populate"
+import z from "zod"
+
+const planetValidator = z.object({
+  name: z.string(),
+  distanceFromStar: z.number().optional(),
+  mass: z.number().optional(),
+  oribitalPeriod: z.number().optional(),
+  diameter: z.number().optional(),
+  moons: z.number().optional(),
+  hasRings: z.boolean().optional(),
+  minTemperature: z.number().optional(),
+  maxTemperature: z.number().optional(),
+  atmosphere: z.string().optional(),
+  details: z.object({
+    density: z.number().optional(),
+    surfaceGravity: z.number().optional(),
+    escapeVelocity: z.number().optional(),
+    rotationPeriod: z.number().optional(),
+    firstRecorded: z.string().optional(),
+    recordedBy: z.string().optional(),
+  }).optional(),
+})
+
 
 // @desc    Fetch all planets
 // @route   GET /api/v1/planets
 // @access  Public
-
 export const getPlanets = asyncHandler(async (req: RequestWithCustomQuery, res, next) => {
   const { include } = req.query
 
@@ -41,4 +63,38 @@ export const getPlanetById = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json(planet)
+})
+
+// @desc    Create new planet
+// @route   POST /api/v1/planets
+// @access  Temporarily Public
+export const createPlanet = asyncHandler(async (req, res, next) => {
+
+  const validatedData = planetValidator.safeParse(req.body)
+
+  console.log(req.body);
+
+  if(!validatedData.success) {
+    const errorMsg = validatedData.error.issues.map((issue) => `${issue.path}: ${issue.message}`).join(" | ")
+    return next(new ErrorResponse(errorMsg, 400))
+  }
+
+  console.log(validatedData);
+
+  const planetData = validatedData.data
+
+  const planet = await prisma.planet.create({
+    data: {
+      ...planetData,
+      details: planetData.details ? { create: planetData.details } : {
+        create:true
+      },
+    }
+  })
+
+  if(!planet) {
+    return next(new ErrorResponse("Planet could not be created", 400))
+  }
+
+  res.status(201).json(planet)
 })
--- src/routes/planets.ts
+++ src/routes/planets.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getPlanets, getPlanetById } from '../controllers/planets';
+import { getPlanets, getPlanetById, createPlanet } from '../controllers/planets';
 import { requestQueryBuilder } from '../middlewares/requestQueryBuilder';
 
 const router = express.Router();
 
-router.route('/').get(requestQueryBuilder, getPlanets)
+router.route('/').get(requestQueryBuilder, getPlanets).post(createPlanet);
 router.route('/:id').get(getPlanetById)
 
 export default router;
